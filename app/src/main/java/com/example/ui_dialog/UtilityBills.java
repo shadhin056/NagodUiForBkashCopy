480b64caa222ddf2902b22cb496fcb6058ad17e6
ShadhiN <shadhinemail@gmail.com>
2019-06-10 12:16:44 +0600

    Icon added

--- app/src/main/java/com/example/ui_dialog/UtilityBills.java
+++ app/src/main/java/com/example/ui_dialog/UtilityBills.java
@@ -7,17 +7,25 @@ import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.Spinner;
 
 public class UtilityBills extends AppCompatActivity {
     Spinner sp_source_account;
     Button btnSubmit;
+    LinearLayout llnext;
+    ImageView ivnext;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_utility_bills);
         sp_source_account = findViewById(R.id.sp_source_account);
         btnSubmit = findViewById(R.id.btnSubmit);
+        ivnext = findViewById(R.id.ivnext);
+        llnext = findViewById(R.id.llnext);
+
+
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(UtilityBills.this,
                 R.array.country, android.R.layout.simple_spinner_item);
 
@@ -29,8 +37,15 @@ public class UtilityBills extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 String action;
-                Intent intent=new Intent(UtilityBills.this,SuccessActivity.class);
+                Intent intent = new Intent(UtilityBills.this, SuccessActivity.class);
                 startActivity(intent);
+            }
+        });
+        ivnext.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                llnext.setVisibility(View.VISIBLE);
+
             }
         });
     }
